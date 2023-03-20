cf6dd1d058ff701543f0dcd132874c2ad4eb01e9
Jonathan Vu <jv1901@g.rit.edu>
2023-03-20 12:29:26 -0400

    adding some search code to rebase

--- SearchByCharacters.java
+++ SearchByCharacters.java
@@ -1,11 +1,25 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class SearchByCharacters implements CollectionSearcher{
 
+    private boolean exactMatch;
+
+    public SearchByCharacters(boolean exactMatch) {
+        this.exactMatch = exactMatch;
+    }
+
     @Override
-    public List<Comic> search(List<Comic> comics) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'search'");
+    public List<Comic> search(List<Comic> comics, String searchTerm) {
+        List<Comic> searchComics = new ArrayList<>();
+        if(exactMatch) {
+
+        } else {
+
+        }
+
+        //in place for now until further clarification
+        return searchComics;
     }
     
 }
--- SearchByCreators.java
+++ SearchByCreators.java
@@ -2,6 +2,12 @@ import java.util.List;
 
 public class SearchByCreators implements CollectionSearcher{
 
+    private boolean exactMatch;
+    
+    public SearchByCreators(boolean exactMatch) {
+        this.exactMatch = exactMatch;
+    }
+    
     @Override
     public List<Comic> search(List<Comic> comics, String searchTerm) {
         // TODO Auto-generated method stub
