52157347bfa72fa5f4ea0203e59012eeca67417c
Jonathan Vu <jv1901@g.rit.edu>
2023-03-22 11:39:16 -0400

    finished all required searches (characters invalid)

--- SearchByTitle.java
+++ SearchByTitle.java
@@ -1,11 +1,31 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class SearchByTitle implements CollectionSearcher{
 
+    private boolean exactMatch;
+
+    public SearchByTitle(boolean exactMatch) {
+        this.exactMatch = exactMatch;
+    }
+    
     @Override
     public List<Comic> search(List<Comic> comics, String searchTerm) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'search'");
+        List<Comic> searchComics = new ArrayList<>();
+        if(this.exactMatch) {
+            for(Comic comic : comics) {
+                if(searchTerm.equals(comic.getSeriesTitle())) {
+                    searchComics.add(comic);
+                }
+            }
+        } else {
+            for(Comic comic : comics) {
+                if(comic.getSeriesTitle().contains(searchTerm)) {
+                    searchComics.add(comic);
+                }
+            }
+        }
+        return searchComics;
     }
     
 }
