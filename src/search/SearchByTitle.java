a7f1f33bd645a049b6e59e93448effa71009ca71
Jonathan Vu <jv1901@g.rit.edu>
2023-03-26 14:29:02 -0400

    implemented the database search method in title

--- src/search/SearchByTitle.java
+++ src/search/SearchByTitle.java
@@ -1,4 +1,8 @@
 package src.search;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -44,10 +48,38 @@ public class SearchByTitle implements CollectionSearcher{
         return searchComics;
     }
 
+    /** 
+     * Searches the database for any comics having a description with
+     * the matching search term
+     * @param filename
+     * @param searchTerm
+     * @return List<Comic>
+     */
     @Override
-    public List<Comic> databaseSearch(String filename, String searchTerm) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'databaseSearch'");
+    public List<Comic> databaseSearch(String searchTerm) {
+        searchTerm = searchTerm.toLowerCase();
+        List<Comic> searchComics = new ArrayList<>();
+        File file = new File(COMIC_DATABASE);
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String line = br.readLine(); //skipping a line
+            while((line = br.readLine()) != null) {
+                String[] split = line.split(",(?=([^\"]*\"[^\"]*\")*[^\"]*$)");
+                    if(exactMatch) {
+                        if(searchTerm.equals(split[2].toLowerCase())) {
+                            searchComics.add(generateComic(split));
+                        }
+                    } else {
+                        if(split[2].toLowerCase().contains(searchTerm)) {
+                           searchComics.add(generateComic(split));
+                        }
+                    }
+                }
+                br.close();
+        } catch (IOException e){
+            System.out.println("Invalid filename.");
+        }
+        return searchComics;
     }
     
 }
