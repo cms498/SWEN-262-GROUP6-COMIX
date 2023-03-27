8d5095d91cceb42c5a9ecf0f5f23da631b5bd61b
DinsonChen <89424863+DinsonChen@users.noreply.github.com>
2023-03-27 18:03:38 -0400

    changed ptui command promots to quotes instead of <>

--- src/PTUI.java
+++ src/PTUI.java
@@ -23,15 +23,15 @@ public class PTUI {
                 + "\nType out all commands in the \033[4;37mproper format\033[0m to properly execute them\n"
                 + "\033[1;31m" + "***********************************************************************" + "\033[0m");
 
-        String quitter = "\033[1;90m>>To end the application -> <quit>\u001B[0m";
-        String PersonalCollectionSearchCommand = "\033[1;90m\u001B[31m>>To search your personal collection -> <search collection>, <search type>, <term>, <exact or partial>";
-        String DataBaseSearchCommand = "\u001B[32m>>To search the database -> <search database>, <search type>, <term>, <exact or partial>";
-        String PersonalCollectionSortCommand = "\u001B[33m>>To sort your personal collection -> <sort>, <sort type>";
-        String AddComicFROMDBtoPersonalCollection = "\u001B[34m>>To add comic from the database to your personal collection -> <add from database>, <exact comic name>";
-        String AddComicManuallytoPersonalCollection = "\u001B[35m>>To add a comic manually to your personal collection-> <add>, <series>, <issue>, <volume>, <title>, <description>, <publisher>, <release date>, <value> <creator1, creator2, ...>";
-        String EditComicInPersonalCollection = "\u001B[36m>>To edit a comic in your personal collection -> <edit>, <exact comic name>, <field to be edited>, <new value>";
-        String GradeComicPersonalCollection = "\u001B[37m>>To grade a comic in your personal collection -> <grade>, <exact comic name>, <value 1 to 10>";
-        String ComicSlab = "\u001B[33m>>To slab a graded comic -> <slab>, <exact comic name>\u001B[0m";
+        String quitter = "\033[1;90m>>To end the application -> \"quit\"\u001B[0m";
+        String PersonalCollectionSearchCommand = "\033[1;90m\u001B[31m>>To search your personal collection -> \"search collection\", <search type>, <term>, <exact or partial>";
+        String DataBaseSearchCommand = "\u001B[32m>>To search the database -> <search database>, \"search type\", <term>, <exact or partial>";
+        String PersonalCollectionSortCommand = "\u001B[33m>>To sort your personal collection -> \"sort\", <sort type>";
+        String AddComicFROMDBtoPersonalCollection = "\u001B[34m>>To add comic from the database to your personal collection -> \"add from database>\", <exact comic name>";
+        String AddComicManuallytoPersonalCollection = "\u001B[35m>>To add a comic manually to your personal collection-> \"add\", <series>, <issue>, <volume>, <title>, <description>, <publisher>, <release date>, <value> <creator1, creator2, ...>";
+        String EditComicInPersonalCollection = "\u001B[36m>>To edit a comic in your personal collection -> \"edit\", <exact comic name>, <field to be edited>, <new value>";
+        String GradeComicPersonalCollection = "\u001B[37m>>To grade a comic in your personal collection -> \"grade\", <exact comic name>, <value 1 to 10>";
+        String ComicSlab = "\u001B[33m>>To slab a graded comic -> \"slab\", <exact comic name>\u001B[0m";
 
         String commands = String.format("\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n",
                 quitter,
