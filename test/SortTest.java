9da4bcafec0d881204feb069ca08e2f58f64c18a
ShreyesGadwalkar <shreyes.gadwalkar@gmail.com>
2023-03-30 20:18:25 -0400

    finished test

--- test/SortTest.java
+++ test/SortTest.java
@@ -31,9 +31,9 @@ public class SortTest {
         creators.add(creator);
         creators.add(creator2);
 
-        Comic comic = new Comic(publisher, "Spider-Man", "SPIDER_MAN_TITLE", 0, "2", "1/2/2019", creators, "A good book", 26.08, false, false);
-        Comic comic2 = new Comic(publisher2, "Batman", "BATMAN", 1, "1", "2/7/2020", creators, "MID", 0, false, false);
-        Comic comic3 = new Comic(publisher, "ANT_MAN", "ANT_MAN", 2, "3", "2/8/2020", creators, "also very mid", 0, false, false);
+        Comic comic = new Comic(publisher, "Spider-Man", "SPIDER_MAN_TITLE", 0, "2", "Jan 2, 2019", creators, "A good book", 26.08, false, false);
+        Comic comic2 = new Comic(publisher2, "Batman", "BATMAN", 1, "1", "Jan 7, 2020", creators, "MID", 0, false, false);
+        Comic comic3 = new Comic(publisher, "ANT_MAN", "ANT_MAN", 2, "3", "Jan 8, 2020", creators, "also very mid", 0, false, false);
       
 
 
