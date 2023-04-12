88b6a5fa6eb6194e8549c890ce21a744fed325e7
Jonathan Vu <jv1901@g.rit.edu>
2023-04-12 19:01:57 -0400

    added some comments to commands

--- src/command/Command.java
+++ src/command/Command.java
@@ -1,5 +1,9 @@
 package src.command;
 
+/*
+ * Command interface, all future commands will implement this interface.
+ * Must be able to execute and undo.
+ */
 public interface Command {
     void execute();
     void undo();
