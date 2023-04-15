d35f2a4a07265fc9b2da4a4eb2700e4e3bcb9129
cms498 <cms8219@rit.edu>
2023-04-15 15:08:21 -0400

    creator and publisher commeneted

--- src/Creator.java
+++ src/Creator.java
@@ -1,25 +1,42 @@
 package src;
+
+/**
+ * This class represents a comic book creators, a comic can have one or multiple
+ * creators
+ */
 public class Creator {
     private String name;
 
-    public Creator(String name){
+    public Creator(String name) {
         this.name = name;
     }
 
+    /**
+     * Used to get the name of the specific creator
+     * 
+     * @return name of the creator
+     */
     public String getName() {
         return name;
     }
-    
+
     @Override
+    /**
+     * This method compares two creators and determines if they are equal,
+     * they are equal if they have the same name
+     */
     public boolean equals(Object o) {
-        if(o instanceof Creator) {
-            Creator other = (Creator)o;
+        if (o instanceof Creator) {
+            Creator other = (Creator) o;
             return this.name.equals(other.name);
         }
         return false;
     }
 
     @Override
+    /**
+     * Used to print out a creator to the console
+     */
     public String toString() {
         return name;
     }
--- src/Publisher.java
+++ src/Publisher.java
@@ -1,25 +1,41 @@
 package src;
+
+/**
+ * This class represents a comic book publisher, each comic has one
+ */
 public class Publisher {
     private String name;
 
-    public Publisher(String name){
+    public Publisher(String name) {
         this.name = name;
     }
 
+    /**
+     * Returns the name of the comic book publisher
+     * 
+     * @return name of publisher
+     */
     public String getName() {
         return name;
     }
 
     @Override
+    /**
+     * This method is used to compared two publishers and see if they are equal
+     * to each other, they are equal if they have the same name
+     */
     public boolean equals(Object o) {
-        if(o instanceof Publisher) {
-            Publisher other = (Publisher)o;
+        if (o instanceof Publisher) {
+            Publisher other = (Publisher) o;
             return this.name.equals(other.name);
         }
         return false;
     }
 
     @Override
+    /**
+     * Used to print out a publisher to the console
+     */
     public String toString() {
         return name;
     }
